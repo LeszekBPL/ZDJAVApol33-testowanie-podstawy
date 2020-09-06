89e09773fd67b9ec5aa1813a95d89ab7a3192523
Leszek B <l.buczek90@gmail.com>
2020-09-06 14:38:03 +0200

    code adjustemen

--- src/main/java/pl/sda/calculator/Calculator.java
+++ src/main/java/pl/sda/calculator/Calculator.java
@@ -15,20 +15,25 @@ public class Calculator {
     }
 
     public int multiplication(int arg1, int agr2) {
-        return arg1*agr2;
+        return arg1 * agr2;
     }
-    public int division (int arg1,int arg2)
-    {if (arg2==0){return 0;}
-    else
-    return  arg1/arg2;
+
+    public int division(int arg1, int arg2) {
+        if (arg2 == 0) {
+            return 0;
+        } else
+            return arg1 / arg2;
     }
-    public boolean isItEven(int arg1){
-        return arg1%2==0;
+
+    public boolean isItEven(int arg1) {
+        return arg1 % 2 == 0;
     }
-    public boolean isOddValue(int arg1){
-        return arg1%2==1;
+
+    public boolean isOddValue(int arg1) {
+        return arg1 % 2 == 1;
     }
-    public double precentageOfNumber(double arg1,double arg2){
-        return (arg1*arg2)/100.0d;
+
+    public double precentageOfNumber(double arg1, double arg2) {
+        return (arg1 * arg2) / 100.0d;
     }
-    }
+}
--- src/test/java/pl/sda/calculator/CalculatorTest.java
+++ src/test/java/pl/sda/calculator/CalculatorTest.java
@@ -45,120 +45,133 @@ class CalculatorTest {
         //then
         assertEquals(14, actual);
     }
+
     @Test
     @DisplayName("Powinno zwrócic 6 jako wynik mnożenia 2 razy 3")
-    void shouldReturnSixAsResultOfMultiplyingArgs(){
+    void shouldReturnSixAsResultOfMultiplyingArgs() {
         //given
-        int arg1=2;
-        int agr2=3;
+        int arg1 = 2;
+        int agr2 = 3;
         //when
-        int actual=sut.multiplication(arg1,agr2);
+        int actual = sut.multiplication(arg1, agr2);
         //then
-        assertEquals(6,actual);
+        assertEquals(6, actual);
     }
+
     @Test
     @DisplayName("Powinno zwrócic 0  jako wynik mnożenia razy 0")
-    void shouldReturnZeroAsResultOfMultiplyingArgs(){
+    void shouldReturnZeroAsResultOfMultiplyingArgs() {
         //given
-        int arg1=0;
-        int agrThatIsNotImportant=3;
+        int arg1 = 0;
+        int agrThatIsNotImportant = 3;
         //when
-        int actual=sut.multiplication(arg1,agrThatIsNotImportant);
+        int actual = sut.multiplication(arg1, agrThatIsNotImportant);
         //then
-        assertEquals(0,actual);
+        assertEquals(0, actual);
     }
+
     @Test
     @DisplayName("Zwraca 3 przy dzieleniu 9 przez 3")
-    void shouldReturnThreeWhenNineIsDividedByThree(){
+    void shouldReturnThreeWhenNineIsDividedByThree() {
         //given
-        int arg1=9;
-        int arg2=3;
+        int arg1 = 9;
+        int arg2 = 3;
         //when
-        int actual=sut.division(arg1,arg2);
+        int actual = sut.division(arg1, arg2);
         //then
-        assertEquals(3,actual);
+        assertEquals(3, actual);
     }
+
     @Test
     @DisplayName("Powinno zwrócić 0 jako wynik dzielenia 0 przez dowolną liczbę")
-    void shouldReturn0WhenZeroIsDevidedByNumber(){
+    void shouldReturn0WhenZeroIsDevidedByNumber() {
         //given
-        int arg1 =0;
-        int argIsNotImportant=1234;
+        int arg1 = 0;
+        int argIsNotImportant = 1234;
         //when
-        int actual=sut.division(arg1,argIsNotImportant);
+        int actual = sut.division(arg1, argIsNotImportant);
         //then
-        assertEquals(0,actual);
-    }@Test
-    @DisplayName("Powinno zwrócić 0 jako wynik dzielenia dowolnej liczby przez 0")
-    void shouldReturn0WhenNumberIsDividedByZero(){
-        //given
-        int argIsNotImportant =1234;
-        int arg2=0;
-        //when
-        int actual=sut.division(argIsNotImportant,arg2);
-        //then
-        assertEquals(0,actual);
+        assertEquals(0, actual);
     }
+
+    @Test
+    @DisplayName("Powinno zwrócić 0 jako wynik dzielenia dowolnej liczby przez 0")
+    void shouldReturn0WhenNumberIsDividedByZero() {
+        //given
+        int argIsNotImportant = 1234;
+        int arg2 = 0;
+        //when
+        int actual = sut.division(argIsNotImportant, arg2);
+        //then
+        assertEquals(0, actual);
+    }
+
     @Test
     @DisplayName("powinno zwrócić prawdę dla liczby parzystej ")
-    void shouldReturnTruthForEvenValue(){
+    void shouldReturnTruthForEvenValue() {
         //given
-        int arg1=6;
+        int arg1 = 6;
         //when
-        boolean actual=sut.isItEven(arg1);
+        boolean actual = sut.isItEven(arg1);
         //then
         assertTrue(actual);
     }
+
     @Test
     @DisplayName("powinno zwrócić falsz dla liczby nieparzystej ")
-    void shouldReturnFalseForOddValue(){
+    void shouldReturnFalseForOddValue() {
         //given
-        int arg1=7;
+        int arg1 = 7;
         //when
-        boolean actual=sut.isItEven(arg1);
+        boolean actual = sut.isItEven(arg1);
         //then
         assertFalse(actual);
-    }@Test
+    }
+
+    @Test
     @DisplayName("powinno zwrócić prawdę dla liczby nieparzystej ")
-    void shouldReturnTruthForOddValue(){
+    void shouldReturnTruthForOddValue() {
         //given
-        int arg1=3;
+        int arg1 = 3;
         //when
-        boolean actual=sut.isOddValue(arg1);
+        boolean actual = sut.isOddValue(arg1);
         //then
         assertTrue(actual);
     }
+
     @Test
     @DisplayName("powinno zwrócić falsz dla liczby parzystej ")
-    void shouldReturnFalseForEvenValue(){
+    void shouldReturnFalseForEvenValue() {
         //given
-        int arg1=2;
+        int arg1 = 2;
         //when
-        boolean actual=sut.isOddValue(arg1);
+        boolean actual = sut.isOddValue(arg1);
         //then
         assertFalse(actual);
     }
+
     @Test
     @DisplayName("powinno zwrócić 2,5 przy liczeniu 50% z liczby 5")
-    void returnsFiftyPercentFromNumber5(){
-    //given
-    double arg1=5;
-    double precentageOfNumber=50.0;
-    //when
-    double actual=sut.precentageOfNumber(arg1,precentageOfNumber);
-    //then
-    assertEquals(2.5,actual,0.00001d);
+    void returnsFiftyPercentFromNumber5() {
+        //given
+        double arg1 = 5;
+        double precentageOfNumber = 50.0;
+        //when
+        double actual = sut.precentageOfNumber(arg1, precentageOfNumber);
+        //then
+        assertEquals(2.5, actual, 0.00001d);
     }
+
     @Test
     @DisplayName("powinno zwrócić 200 przy liczeniu 10% z 2000")
-    void returnsTenthPercentFromTwoTousnds(){
+    void returnsTenthPercentFromTwoTousnds() {
         //given
-        double arg1=2000;
-        double precentageOfNumber=10;
+        double arg1 = 2000;
+        double precentageOfNumber = 10;
         //when
-        double actual=sut.precentageOfNumber(arg1,precentageOfNumber);
+        double actual = sut.precentageOfNumber(arg1, precentageOfNumber);
         //then
-        assertEquals(2.5,actual,0.00001d);
+        assertEquals(2.5, actual, 0.00001d);
     }
 
 }
